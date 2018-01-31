b69938092897cd20c3c2f0b3ee291160ea4ab7d9
Elmer Bulthuis <elmerbulthuis@gmail.com>
2018-01-31 21:35:35 +0100

    upgrade

--- src/synchronize.spec.ts
+++ src/synchronize.spec.ts
@@ -1,70 +1,67 @@
-import * as test from "tape";
-import { synchronize, join } from "./synchronize";
+// tslint:disable:max-classes-per-file
 
+import * as test from "tape";
+import { join, synchronize } from "./synchronize";
 
 class Count {
 
-    numbers = ["zero", "one", "two", "three"];
+    public numbers = ["zero", "one", "two", "three"];
 
     @synchronize()
-    async one() {
-        await new Promise(resolve => setTimeout(resolve, 300));
+    public async one() {
+        await new Promise((resolve) => setTimeout(resolve, 300));
         return "1";
     }
 
     @synchronize()
-    async two() {
-        await new Promise(resolve => setTimeout(resolve, 200));
+    public async two() {
+        await new Promise((resolve) => setTimeout(resolve, 200));
         return "2";
     }
 
     @synchronize()
-    async three() {
-        await new Promise(resolve => setTimeout(resolve, 100));
+    public async three() {
+        await new Promise((resolve) => setTimeout(resolve, 100));
         return "3";
     }
 
     @synchronize()
-    async num(n: number) {
-        await new Promise(resolve => setTimeout(resolve, 400));
+    public async num(n: number) {
+        await new Promise((resolve) => setTimeout(resolve, 400));
         return this.numbers[n];
     }
 }
 
-
-
 class SillyCalculatorExample {
-    current = 0;
+    public current = 0;
 
     @synchronize()
-    async add(value) {
-        await new Promise(resolve => setTimeout(resolve, 2000));
+    public async add(value: number) {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
         this.current += value;
         return this.current;
     }
 
 }
 
-
-
-test("synchronize", async t => {
-    const l = [];
+test("synchronize", async (t) => {
+    const l = new Array<string>();
 
     const c = new Count();
-    c.one().then(v => l.push(v));
+    c.one().then((v) => l.push(v));
     t.deepEqual(l, []);
 
-    c.two().then(v => l.push(v));
+    c.two().then((v) => l.push(v));
     t.deepEqual(l, []);
 
-    await c.three().then(v => l.push(v));
+    await c.three().then((v) => l.push(v));
     t.deepEqual(l, ["1", "2", "3"]);
 
-    c.num(0).then(v => l.push(v));
-    await c.num(1).then(v => l.push(v));
+    c.num(0).then((v) => l.push(v));
+    await c.num(1).then((v) => l.push(v));
 
-    c.num(2).then(v => l.push(v));
-    c.num(3).then(v => l.push(v));
+    c.num(2).then((v) => l.push(v));
+    c.num(3).then((v) => l.push(v));
 
     await join(c);
     t.deepEqual(l, ["1", "2", "3", "zero", "one", "two", "three"]);
@@ -72,20 +69,19 @@ test("synchronize", async t => {
     t.end();
 });
 
-
-test("silly calculator", async t => {
+test("silly calculator", async (t) => {
     const calculator = new SillyCalculatorExample();
-    calculator.add(5).then(value => {
+    calculator.add(5).then((value) => {
         // value === 5
         t.equal(value, 5);
     });
-    calculator.add(5).then(value => {
+    calculator.add(5).then((value) => {
         // value === 10 (!)
         t.equal(value, 10);
     });
 
     // also, you can get the result of the last promise with the join function
-    join(calculator).then(value => {
+    join(calculator).then((value) => {
         t.equal(value, 10);
     });
 
--- src/synchronize.ts
+++ src/synchronize.ts
@@ -1,33 +1,44 @@
 const promiseIndex = new WeakMap<any, Promise<any>>();
 
 /**
- * A decorator to make asynchronous methods synchronous. The method will be invoked only after all other synchronized methods are finished.
+ * A decorator to make asynchronous methods synchronous. The method will be
+ * invoked only after all other synchronized methods are finished.
  */
 export function synchronize() {
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    return function synchronizeDetonator(
+        target: any,
+        propertyKey: string,
+        descriptor: PropertyDescriptor,
+    ) {
         const original = descriptor.value;
-        descriptor.value = function (...args) {
+        descriptor.value = function synchronizedMethod(...args: any[]) {
             return synchronizeCall.call(this, this, original, ...args);
         };
     };
 }
 
 /**
- * Synchronized call to a method. The method is called after all other synchronized calls in the same context are finished.
- * 
+ * Synchronized call to a method. The method is called after all other
+ * synchronized calls in the same context are finished.
+ *
  * @param context The context in wich the methods are synchronized
  * @param fn the (asynchronous) method to synchronize
  * @param args arguments to when calling the method
  */
-export function synchronizeCall<TResult>(context: any, fn: (...args: any[]) => Promise<TResult> | TResult, ...args: any[]): Promise<TResult> {
-    const promise = join(context).then(() => fn.call(this, ...args));
+export function synchronizeCall<TResult>(
+    context: any,
+    fn: (...args: any[]) => Promise<TResult> | TResult,
+    ...args: any[],
+): Promise<TResult> {
+    const promise = join(context).then(() => fn.call(context, ...args));
     promiseIndex.set(context, promise);
     return promise;
 }
 
 /**
- * Wait for a context to finish it's synchronized function calls and return the last result.
- * 
+ * Wait for a context to finish it's synchronized function calls and return
+ * the last result.
+ *
  * @param context The synchronization context
  */
 export function join(context: any): Promise<void> {
